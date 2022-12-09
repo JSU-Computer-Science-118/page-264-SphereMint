adcb8bcaa994246cdd2ac4720a4963ac39bc8974
SphereMint <83474707+SphereMint@users.noreply.github.com>
2022-12-09 15:20:37 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,32 @@
-// write your code here
+// Gannon Moore || J00965925 || November 14, 2022
+// Page 264 - Prime Numbers
 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int i, num;
+  bool primeTest = true;
+
+  cout << "Enter a positive integer: ";
+  cin >> num;
+
+  if (num == 1 || num == 1) {
+    primeTest = false;
+  }
+
+  for (i = 2; i <= num/2; ++i) {
+    if ((num % i) == 0) {
+      primeTest = false;
+      break;
+    }
+  }
+
+  if (primeTest)
+    cout << n << " is a prime number";
+  else
+    cout << n << " is not a prime number";
+
+  return 0;
+}
